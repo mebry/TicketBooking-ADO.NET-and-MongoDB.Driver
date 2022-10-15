9604b93820183d24bf31c9849567aa20efb685f8
Mebry <vadushko@gmail.com>
2022-10-15 23:25:05 +0300

    Add a interface IBaseResponse

---
+++ Booking.Domain/Interfaces/IBaseResponse.cs
@@ -0,0 +1,18 @@
+﻿using Booking.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Interfaces
+{
+    public interface IBaseResponse<T>
+    {
+        T Data { get; set; }
+
+        public string Description { get; set; }
+
+        public StatusCode StatusCode { get; set; }
+    }
+}
