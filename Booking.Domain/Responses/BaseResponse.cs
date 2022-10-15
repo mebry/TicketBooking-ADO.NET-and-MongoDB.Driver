69f6eb7dcd287f8ff68f66db0ba70b0813c01aad
Mebry <vadushko@gmail.com>
2022-10-15 23:26:02 +0300

    Create a new folder and a new class

---
+++ Booking.Domain/Responses/BaseResponse.cs
@@ -0,0 +1,19 @@
+﻿using Booking.Domain.Enums;
+using Booking.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Responses
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public string Description { get; set; }
+
+        public StatusCode StatusCode { get; set; }
+
+        public T Data { get; set; }
+    }
+}
