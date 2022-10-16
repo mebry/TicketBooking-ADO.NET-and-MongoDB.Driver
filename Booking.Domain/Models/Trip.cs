53e1a68069bcd2039fee674fccfc7d58e8ec3cf3
Mebry <vadushko@gmail.com>
2022-10-16 23:43:21 +0300

    Add a new class

---
+++ Booking.Domain/Models/Trip.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Models
+{
+    public class Trip
+    {
+        public int TripId { get; set; }
+
+        public string Plane { get; set; }
+
+        public string StartCity { get; set; }
+
+        public string StartCountry { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string EndCity { get; set; }
+
+        public string EndCountry { get; set; }
+    }
+}
---
+++ TicketBooking/Booking.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<T> GetById(int id);
+
+        Task<bool> Create(T entity);
+
+        Task<List<T>> GetAll();
+
+        Task<bool> Delete(T entity);
+
+        Task<bool> Update(T entity);
+    }
+}
