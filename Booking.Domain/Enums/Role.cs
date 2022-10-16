bd384ea079d4e5a55eae6246ac18f61991d38229
Mebry <vadushko@gmail.com>
2022-10-17 02:43:03 +0300

    Add enum

---
+++ Booking.Domain/Enums/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Enums
+{
+    public enum Role
+    {
+        Client = 1,
+        Dispatcher,
+        Admin,
+        Onwer
+    }
+}
