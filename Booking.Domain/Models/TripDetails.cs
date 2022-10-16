8ac986bc9060c90bfe412a1c9c3af754b0659fdb
Mebry <vadushko@gmail.com>
2022-10-16 23:49:52 +0300

    Add several classes

---
+++ Booking.Domain/Models/TripDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.Models
+{
+    public  class TripDetails
+    {
+        public int TripDetailsId { get; set; }
+
+        public int TripId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int Place { get; set; }
+    }
+}
