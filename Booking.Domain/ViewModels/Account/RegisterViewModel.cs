94c6e0688924caea255a5f9a892c1643ae5ddb23
Mebry <vadushko@gmail.com>
2022-10-17 19:49:19 +0300

    Add two viewModels

---
+++ Booking.Domain/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.ViewModels.Account
+{
+    public class RegisterViewModel
+    {
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirm { get; set; }
+
+        public string Codeword { get; set; }
+    }
+}
