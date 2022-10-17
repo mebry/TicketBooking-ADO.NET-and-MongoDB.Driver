15c136808bc7c217a4dff1139dd407d7f45beeee
Mebry <vadushko@gmail.com>
2022-10-17 21:01:11 +0300

    Rename a property

--- Booking.Domain/ViewModels/Account/RegisterViewModel.cs
+++ Booking.Domain/ViewModels/Account/RegisterViewModel.cs
@@ -9,7 +9,7 @@ namespace Booking.Domain.ViewModels.Account
 {
     public class RegisterViewModel
     {
-        public string Name { get; set; }
+        public string UserName { get; set; }
 
         public string Password { get; set; }
 
