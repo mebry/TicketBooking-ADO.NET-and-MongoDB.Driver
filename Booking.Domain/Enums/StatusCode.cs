190ffde7f1488698126a06eb932a9aa57c71f435
Mebry <vadushko@gmail.com>
2022-10-17 23:47:58 +0300

    Update a enum

--- Booking.Domain/Enums/StatusCode.cs
+++ Booking.Domain/Enums/StatusCode.cs
@@ -8,5 +8,16 @@ namespace Booking.Domain.Enums
 {
     public enum StatusCode
     {
+        UserNotFound = 1,
+        UserAlreadyExists,
+        IncorrectPassword,
+        IncorrectCodeword,
+        RoleNotFound,
+        PlaneNotFound,
+        TripNotFound,
+        CitynotFound,
+        CountryNotFound,
+        OK = 200,
+        InternalServerError = 500,
     }
 }
