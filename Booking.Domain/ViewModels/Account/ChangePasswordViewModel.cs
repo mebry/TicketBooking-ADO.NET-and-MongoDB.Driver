a297a5331cf60af96e9ba3d53b6c947dfbc0179c
Mebry <vadushko@gmail.com>
2022-10-17 19:59:42 +0300

    Add a new viewModel

---
+++ Booking.Domain/ViewModels/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.ViewModels.Account
+{
+    public class ChangePasswordViewModel
+    {
+        public string UserName { get; set; }
+
+        public string OldPassword { get; set; }
+
+        public string NewPassword { get; set; }
+
+        public string Codeword { get; set; }
+    }
+}
