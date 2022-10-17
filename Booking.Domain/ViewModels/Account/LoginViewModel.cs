db6cae8278f550b3e522170ae1a9b39340d6a2cf
Mebry <vadushko@gmail.com>
2022-10-17 23:50:19 +0300

    Fix. Rename a property

--- Booking.Domain/ViewModels/Account/LoginViewModel.cs
+++ Booking.Domain/ViewModels/Account/LoginViewModel.cs
@@ -8,7 +8,7 @@ namespace Booking.Domain.ViewModels.Account
 {
     public class LoginViewModel
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
 
         public string Password { get; set; }
     }
