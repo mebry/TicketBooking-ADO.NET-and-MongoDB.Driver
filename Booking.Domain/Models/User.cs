68b47c54072e191d424399262d4ba3d667815d23
Mebry <vadushko@gmail.com>
2022-10-17 19:59:11 +0300

    Rename a property

--- Booking.Domain/Models/User.cs
+++ Booking.Domain/Models/User.cs
@@ -10,7 +10,7 @@ namespace Booking.Domain.Models
     {
         public int Id { get; set; }
 
-        public string Username { get; set; }
+        public string UserName { get; set; }
 
         public string Password { get; set; }
 
