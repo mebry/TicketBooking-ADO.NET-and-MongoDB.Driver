dca68f0f22d0fb6475b00ab05726e9279d3da708
Mebry <vadushko@gmail.com>
2022-10-18 01:31:11 +0300

    Add 2 interfaces

---
+++ Booking.DAL/Interfaces/IPlaneRepository.cs
@@ -0,0 +1,14 @@
+﻿using Booking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.DAL.Interfaces
+{
+    public interface IPlaneRepository : IBaseRepository<Plane>
+    {
+        Task<Plane> GetByPlaneName(string planeName);
+    }
+}
---
+++ Booking.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Booking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.DAL.Interfaces
+{
+    public interface IUserRepository:IBaseRepository<User>
+    {
+        Task<User> GetByUserName(string userName);
+        Task<bool> UpdatePassword(int userId,string newPassword);
+    }
+}
