a421f488ff1ad241914e18f8af26807371a48858
Mebry <vadushko@gmail.com>
2022-10-18 23:10:58 +0300

    Update a interface. Add a new method

---
+++ Booking.DAL/Interfaces/IUserDetailsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Booking.Domain.Models;
+
+namespace Booking.DAL.Interfaces
+{
+    public interface IUserDetailsRepository : IBaseRepository<UserDelails>
+    {
+        Task<bool> UpdateFirstName(int userId, string firstName);
+        Task<bool> UpdateLastName(int userId, string lastName);
+        Task<bool> UpdatePatronymic(int userId, string patronymic);
+        Task<UserDelails> GetByUserName(string userName);
+    }
+}
