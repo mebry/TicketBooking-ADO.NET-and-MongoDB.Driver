7938abdba388c964cd6e68cb6d10b3497cd382f7
Mebry <vadushko@gmail.com>
2022-10-18 23:20:37 +0300

    Add 2 service interfaces

---
+++ Booking.Service/Interfaces/IUserDetailsService.cs
@@ -0,0 +1,19 @@
+﻿using Booking.Domain.Models;
+using Booking.Domain.Responses;
+using Booking.Domain.ViewModels.Account;
+
+namespace Booking.Service.Interfaces
+{
+    public interface IUserDetailsService 
+    {
+        Task<BaseResponse<bool>> Create(UserDelails model);
+        Task<BaseResponse<bool>> Update(UserDelails model);
+        Task<BaseResponse<bool>> DeleteByUserId(int id);
+        Task<BaseResponse<UserDelails>> GetByUserId(int id);
+        Task<BaseResponse<List<UserDelails>>> GetAll();
+        Task<BaseResponse<UserDelails>> GetByUserName(string userName);
+        Task<BaseResponse<bool>> UpdateFirstName(int userId, string firstName);
+        Task<BaseResponse<bool>> UpdateLastName(int userId, string lastName);
+        Task<BaseResponse<bool>> UpdatePatronymic(int userId, string patronymic);
+    }
+}
---
+++ Booking.Service/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Booking.Domain.Models;
+using Booking.Domain.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<BaseResponse<User>> GetById(int id);
+        Task<BaseResponse<List<User>>> GetAll();
+        Task<BaseResponse<bool>> Update(User model);
+    }
+}
