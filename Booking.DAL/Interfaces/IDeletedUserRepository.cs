95ce386140b1e0e89605cad44decd73e813616f9
Mebry <vadushko@gmail.com>
2022-10-18 18:39:10 +0300

    Add a new interface. Name: IDeletedUserRepository

---
+++ Booking.DAL/Interfaces/IDeletedUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Booking.Domain.Models;
+
+namespace Booking.DAL.Interfaces
+{
+    public interface IDeletedUserRepository: IBaseRepository<DeletedUser>
+    {
+        Task<bool> UpdateTheReason(int userId, string reason);
+    }
+}
