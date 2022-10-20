066ddc00421bb6afbf9d82234e3621608b5b34cb
Mebry <vadushko@gmail.com>
2022-10-20 15:10:04 +0300

    Add 2 view models

---
+++ Booking.Domain/ViewModels/Trip/TripInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.ViewModels.Trip
+{
+    public class TripInfo
+    {
+        public int TripId { get; set; }
+
+        public string StartCity { get; set; }
+
+        public string EndCity { get; set; }
+
+        public string StartCountry { get; set; }
+
+        public string EndCountry { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int NumberOfOrderedTickets { get; set; }
+        public int AllTictets { get; set; }
+
+        public int Profit { get; set; }
+    }
+}
---
+++ Booking.Domain/ViewModels/Trip/UserTripViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Domain.ViewModels.Trip
+{
+    public class UserTripViewModel
+    {
+        public int TripId { get; set; }
+
+        public string StartCity { get; set; }
+
+        public string EndCity { get; set; }
+
+        public string StartCountry { get; set; }
+
+        public string EndCountry { get; set; }
+
+        public int Place { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
