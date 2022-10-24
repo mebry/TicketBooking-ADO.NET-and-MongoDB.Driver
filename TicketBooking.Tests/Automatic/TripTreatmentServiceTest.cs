397cb173bf64322be88196444463683bd41cd41a
Mebry <vadushko@gmail.com>
2022-10-24 03:11:47 +0300

    add a new class for testing

---
+++ TicketBooking.Tests/Automatic/TripTreatmentServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TicketBooking.Tests.Automatic
+{
+    public class TripTreatmentServiceTest
+    {
+        /*[Fact]
+        public void Test1()
+        {
+
+        }*/
+    }
+}
