7e43e74aeaa19f0bbb1c1410e58d98021f318e88
Mebry <vadushko@gmail.com>
2022-10-24 03:34:23 +0300

    Update a new test

--- TicketBooking.Tests/Automatic/TripTreatmentServiceTest.cs
+++ TicketBooking.Tests/Automatic/TripTreatmentServiceTest.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Booking.Domain.Enums;
+using Booking.Domain.Models;
+using Booking.Service.Implementations.Calculations;
+using Booking.Service.Interfaces.Calculations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +13,62 @@ namespace TicketBooking.Tests.Automatic
 {
     public class TripTreatmentServiceTest
     {
-        /*[Fact]
-        public void Test1()
+        [Fact]
+        public void GetAvailablePlaces_NullData_IsFalse()
         {
+            // Arrange
+            ITripTreatmentService tripTreatmentService = new TripTreatmentService();
+            List<TripDetails> tripDetails = null;
+            int capacity = 10;
+            // Act
+            var result = tripTreatmentService.GetAvailablePlaces(tripDetails, capacity);
 
-        }*/
+            // Assert
+            Assert.False(StatusCode.OK == result.StatusCode);
+        }
+
+        [Fact]
+        public void GetAvailablePlaces_IncorrentCount_IsFalse()
+        {
+            // Arrange
+            ITripTreatmentService tripTreatmentService = new TripTreatmentService();
+            List<TripDetails> tripDetails = new List<TripDetails>()
+            {
+                new TripDetails(){Place=1},
+                new TripDetails(){Place=3},
+                new TripDetails(){Place=2},
+                new TripDetails(){Place=5},
+                new TripDetails(){Place=10},
+            };
+            int capacity = 10;
+            int available = 4;
+            // Act
+            var result = tripTreatmentService.GetAvailablePlaces(tripDetails, capacity);
+
+            // Assert
+            Assert.False(available == result.Data.Count);
+        }
+
+        [Fact]
+        public void GetAvailablePlaces_CorrentCount_IsTrue()
+        {
+            // Arrange
+            ITripTreatmentService tripTreatmentService = new TripTreatmentService();
+            List<TripDetails> tripDetails = new List<TripDetails>()
+            {
+                new TripDetails(){Place=1},
+                new TripDetails(){Place=3},
+                new TripDetails(){Place=2},
+                new TripDetails(){Place=5},
+                new TripDetails(){Place=10},
+            };
+            int capacity = 10;
+            int available = 5;
+            // Act
+            var result = tripTreatmentService.GetAvailablePlaces(tripDetails, capacity);
+
+            // Assert
+            Assert.True(available == result.Data.Count);
+        }
     }
 }
